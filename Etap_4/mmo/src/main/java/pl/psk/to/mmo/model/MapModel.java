5a1a54a376b98d903f65cd66820b74e7be9dae28
Dominik Kaliszewski <dkaliszewski@pretius.com>
2021-04-16 08:37:01 +0200

    Add missing annotations

--- Etap_4/mmo/src/main/java/pl/psk/to/mmo/model/MapModel.java
+++ Etap_4/mmo/src/main/java/pl/psk/to/mmo/model/MapModel.java
@@ -1,8 +1,12 @@
 package pl.psk.to.mmo.model;
 
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import pl.psk.to.mmo.model.base.Auditable;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class MapModel extends Auditable {
     private Long id;
     private String name;
